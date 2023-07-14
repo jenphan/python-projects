71df92f433b91118feae91ff7990865e6aea425a
Jennifer Phan <56607243+jenphan@users.noreply.github.com>
2023-07-14 15:06:55 -0400

    Create dice_simulator.py

---
+++ dice_simulator.py
@@ -0,0 +1,37 @@
+import random
+import sys
+
+dice = [1, 2, 3, 4, 5, 6]
+history = []
+
+def intro_msg():
+    print("(´• ω •`) ♡ WELCOME TO THE DICE ROLLING SIMULATOR! (´ε｀ )♡")
+    print("type 'yes' or 'y' to roll.")
+
+def roll_dice():
+    roll = random.choice(dice)
+    history.append(roll)
+    print(f"> You rolled a {roll}. (♡˙︶˙♡)")
+
+def statistics(rolled):
+    stats  = [0] * 6
+
+    for num in rolled:
+        stats[num - 1] += 1
+
+    total = sum(stats)
+
+    print("\n｡ﾟ( ﾟ^∀^ﾟ)ﾟ｡Thank you for playing!")
+    for side in dice:
+        print(f"> {side}'s Rolled: {stats[side - 1]}/{total}")
+
+intro_msg()
+
+while True:
+    PLAYING = input("\nRoll the dice? ")
+
+    if PLAYING in ["yes", "y"]:
+        roll_dice()
+    else:
+        statistics(history)
+        sys.exit()
