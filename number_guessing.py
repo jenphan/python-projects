9b49bf2fcfd9c0d378ad5a73f91cafa41644c312
Jen <jdp386@drexel.edu>
2023-07-18 13:38:30 -0400

    Add hint feature when guessing incorrectly

--- number_guessing.py
+++ number_guessing.py
@@ -3,23 +3,32 @@ import sys
 
 INTRO = """(´• ω •`) ♡ WELCOME TO THE NUMBER GUESSING GAME! (´ε｀ )♡
 You can guess a number from 1 to 10.
-Type 'hint' or 'h' for a hint.
+Guessing incorrectly will reduce your score.
 Type 'quit' or 'q' to quit.
 """
 
-def validate_input(userinput):
+def validate_input(guess):
     result = -1
     try:
-        num = int(userinput)
+        num = int(guess)
         result = num
     except ValueError:
-        if userinput in ['quit', 'q', 'hint', 'h']:
-            result = userinput;
+        if guess in ['quit', 'q']:
+            result = guess
     return result
 
-def check_result(actual, guess):
-    if actual == guess:
-        print(f"\nYou guessed the secret number! {rand}")
+def give_hint(score):
+    score -= 10
+    if GUESS > rand:
+        print("HINT: The secret number is lower than your guess.")
+    else:
+        print("HINT: The secret number is greater than your guess.")
+    return score
+
+def check_result():
+    if rand == GUESS:
+        print(f"\nYou guessed the secret number: {rand}/100!")
+        print(f"Your FINAL SCORE is {SCORE}!")
         print("Would you like to play again? (y/n)")
         again = input("> ")
         if again not in ['yes', 'y']:
@@ -34,18 +43,21 @@ def exit_game():
 
 while True:
     print(INTRO)
+
     PLAYING = True
     GUESS = ""
+    SCORE = 100
+
     rand = random.randint(1, 10)
 
     while PLAYING:
+        
         GUESS = validate_input(input("> "))
         if GUESS in ['quit', 'q']:
             print(f"\nThe secret number was {rand}!")
             exit_game()
-        elif GUESS in ['hint', 'h']:
-            print("Hint!")
         elif GUESS <= 0 or GUESS >= 11:
             print("\nPlease guess a number from 1 to 10.")
         else:
-            PLAYING = check_result(rand, GUESS)
+            PLAYING = check_result()
+            SCORE = give_hint(SCORE)
