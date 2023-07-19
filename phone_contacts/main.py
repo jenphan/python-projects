8bae00d39b518cb7bb0b495f09681939a651c039
Jen <jdp386@drexel.edu>
2023-07-19 16:28:16 -0400

    Add create contact function

--- phone_contacts/main.py
+++ phone_contacts/main.py
@@ -28,7 +28,7 @@ def create_addr(conn, address):
     return cur.lastrowid
 
 def create_contact(conn, contact):
-    sql = ''' INSERT INTO contacts(first_name,last_name,addr_id,phone_number,email_addr)
+    sql = ''' INSERT INTO contacts(phone_number,first_name,last_name,addr_id,email_addr)
                 VALUES(?,?,?,?,?)'''
     cur = conn.cursor()
     cur.execute(sql, contact)
@@ -40,21 +40,21 @@ def main():
 
     sql_create_contacts_table = """ CREATE TABLE IF NOT EXISTS contacts (
                                     [contact_id] INTEGER PRIMARY KEY,
+                                    [phone_number] NVARCHAR(10) NOT NULL,
                                     [first_name] NVARCHAR(50) NOT NULL,
                                     [last_name] NVARCHAR(50) NULL,
-                                    [addr_id] INTEGER NOT NULL,
-                                    [phone_number] NVARCHAR(255) NOT NULL,
-                                    [email_addr] NVARCHAR(255) NOT NULL,
+                                    [addr_id] INTEGER NULL,
+                                    [email_addr] NVARCHAR(255) NULL,
                                     FOREIGN KEY(addr_id) REFERENCES contacts(addr_id)
                                 );"""
     
     sql_create_address_table = """ CREATE TABLE IF NOT EXISTS address (
                                     [addr_id] INTEGER PRIMARY KEY,
-                                    [addr] NVARCHAR(255) NULL,
-                                    [addr2] NVARCHAR(255) NULL,
-                                    [city] NVARCHAR(255) NULL,
-                                    [state] NVARCHAR(255) NULL,
-                                    [zipcode] NVARCHAR(5)
+                                    [addr] NVARCHAR(100) NULL,
+                                    [addr2] NVARCHAR(100) NULL,
+                                    [city] NVARCHAR(60) NULL,
+                                    [state] NVARCHAR(15) NULL,
+                                    [zipcode] NVARCHAR(5) NULL
                                 )
                                 """
     
@@ -65,13 +65,24 @@ def main():
         create_table(conn, sql_create_address_table)
     else:
         print("Error! cannot create the database connection.")
-    
-    with conn:
-        addr = ('ADDRESS 1', 'ADDRESS 2' , 'CITY', 'STATE', 'ZIPCODE')
-        addr_id = create_addr(conn, addr)
 
-        new_contact = ('FIRST NAME', 'LAST NAME', addr_id, 'PHONE NUMBER', 'EMAIL ADDRESS')
+def add_contact():
+    phone_number = input("Please enter in the phone number (no dashes)\n> ")
+    first_name = input("Please enter in the first name.\n> ")
+    last_name = input("Please enter in the last name.\n> ")
+    addr_1 = input("Please enter in the first address line.\n> ")
+    addr_2 = input("Please enter in additional address information.\n> ")
+    city = input("Please enter in the city.\n> ")
+    state = input("Please enter in the state.\n> ")
+    zipcode = input("Please enter in the zipcode.\n> ")
+    email_addr = input("Please enter in the email address.\n> ")
+
+    with conn:
+        addr_id = create_addr(conn, (addr_1, addr_2 , city, state, zipcode))
+
+        new_contact = (phone_number, first_name, last_name, addr_id, email_addr)
         create_contact(conn, new_contact)
 
 if __name__ == "__main__":
     main()
+    add_contact()
